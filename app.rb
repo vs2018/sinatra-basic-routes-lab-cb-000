68fd96a9dc8ec4ee7ebb9b31f19972335796299f
Vishal Sharma <vsh1819@gmail.com>
2018-07-11 11:34:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Vishal Sharma"
+  end
+  
+  get '/hometown' do
+    "My hometown is London"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Hello"
+  end
+  
+  
+  
+  
+
+
 end
